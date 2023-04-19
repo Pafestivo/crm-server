f8ad5fea309ab3a7b9fd7bd02e5cc1262bf9b534
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-19 10:00:08 +0300

    Add cron-job with node-cron

---
+++ api/sms.js
@@ -0,0 +1,73 @@
+const schedule = require('node-schedule');
+const axios = require('axios');
+const qs = require('qs');
+const scheduleUrl = 'http://localhost:3000/schedules';
+const timeFormat = {
+  year: '2-digit',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  hourCycle: 'h23'
+}
+
+
+const getSchedules = async () => {
+  const response = await fetch(scheduleUrl, {
+    method: 'GET'
+  });
+  const schedules = await response.json();
+  return schedules;
+}
+
+const deleteSchedule = async (id) => {
+  const response = await fetch(`${scheduleUrl}/${id}`, {
+    method: 'DELETE'
+  });
+  const schedule = await response.json();
+  return schedule;
+}
+
+const job = schedule.scheduleJob('* * * * *', async () => {
+
+  
+  const schedules = await getSchedules();
+
+  const now = new Date().toLocaleString(undefined, timeFormat);
+  const [currentDate, currentTime] = now.split(', ');
+
+  schedules.forEach(schedule => {
+    if(currentDate === schedule.date && currentTime === schedule.time) {
+
+      let data = qs.stringify({
+        'post': '2',
+        'token': 'VjI45b92301db7cace98d736feb8c05ff2c',
+        'msg': `This is a reminder that ${schedule.customer} has requested a call today at ${schedule.time}. View customer here: ${schedule.customerUrl}`,
+        'list': '0523453336',
+        'from': 'wecome'
+      });
+    
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://www.micropay.co.il/ExtApi/ScheduleSms.php',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data : data
+      };
+
+      axios.request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      deleteSchedule(schedule.id);
+    }
+  });
+});
+
+exports.job = job;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cron = require("node-cron");
+const job = require('./api/sms.js')
 
 const jsonServer = require("json-server")
 const server = jsonServer.create()
@@ -11,43 +11,13 @@ server.use(router)
 
 server.listen(port)
 
-const scheduleUrl = 'https://crm-server-2ja0.onrender.com/schedules';
-const timeFormat = {
-  year: '2-digit',
-  month: '2-digit',
-  day: '2-digit',
-  hour: '2-digit',
-  minute: '2-digit',
-  hourCycle: 'h23'
-}
+job
 
-const getSchedules = async () => {
-  const response = await fetch(scheduleUrl, {
-    method: 'GET'
-  });
-  const schedules = await response.json();
-  return schedules;
-}
 
-const deleteSchedule = async (id) => {
-  const response = await fetch(`${scheduleUrl}/${id}`, {
-    method: 'DELETE'
-  });
-  const schedule = await response.json();
-  return schedule;
-}
 
-cron.schedule('* * * * *', async () => {
 
-  const schedules = await getSchedules();
 
-  const now = new Date().toLocaleString(undefined, timeFormat);
-  const [currentDate, currentTime] = now.split(', ');
 
-  schedules.forEach(schedule => {
-    if(currentDate === schedule.date && currentTime === schedule.time) {
-      console.log(`This is a reminder that ${schedule.customer} has requested a call today at ${schedule.time}. His phone number is ${schedule.customerPhone}`)
-    }
-    deleteSchedule(schedule.id);
-  });
-});
+
+
+
