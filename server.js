f6abaed70ad83bd69ca9bb183896da4d54c5b79a
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-23 14:25:03 +0300

    Remove app.listen

--- server.js
+++ server.js
@@ -33,7 +33,3 @@ app.use((err, req, res, next) => {
 app.use((req, res) => {
   res.status(404).send('404 page not found');
 });
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
